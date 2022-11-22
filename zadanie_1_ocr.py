516e1eebc6d791a3c49f75a42b486bd821e986d7
BBrzek <bartlomiej.brzek@edu.uekat.pl>
2022-11-22 16:49:55 +0100

    zad10 - treshold and adaptive treshold

--- zadanie_1_ocr.py
+++ zadanie_1_ocr.py
@@ -14,6 +14,16 @@ def pic_to_text(file_path):
     cv2.destroyAllWindows()
     '''
 
-path = r'images\5.jpg'
+path = r'images\4.jpg'
 text = pic_to_text(file_path=path)
 print(text)
+
+def test_pic(file_path):
+    img = cv2.imread(file_path)
+    gray_pic = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    converted_img = cv2.adaptiveThreshold(cv2.medianBlur(gray_pic, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    cv2.imshow('image', converted_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+test_pic(file_path=path)

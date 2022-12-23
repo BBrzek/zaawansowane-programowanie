609a27242701238233cdb724abef7b9f05a70b61
unknown <brzek.prog@gmail.com>
2022-12-24 00:55:32 +0100

    gpu test

---
+++ services/cpu_gpu.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+
+def cpu(image_rgb, model):
+    with tf.device('/cpu:0'):
+        img_tf = tf.convert_to_tensor(image_rgb, dtype=tf.uint8)
+        img_tf = tf.expand_dims(img_tf, 0)
+        boxes, scores, classes, num_detections = model(img_tf)
+        return [boxes, scores, classes, num_detections]
+
+
+def gpu(image_rgb,model):
+    with tf.device('/device:GPU:0'):
+        img_tf = tf.convert_to_tensor(image_rgb, dtype=tf.uint8)
+        img_tf = tf.expand_dims(img_tf, 0)
+        boxes, scores, classes, num_detections = model(img_tf)
+        return [boxes, scores, classes, num_detections]
